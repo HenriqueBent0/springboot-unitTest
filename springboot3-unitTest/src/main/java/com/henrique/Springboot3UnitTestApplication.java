4799bffd637862934b77f772abcacc458d12fe24
Henrique Bento <166830118+HenriqueBent0@users.noreply.github.com>
2025-01-28 22:40:24 -0300

    springboot_Junit

---
+++ springboot3-unitTest/src/main/java/com/henrique/Springboot3UnitTestApplication.java
@@ -0,0 +1,13 @@
+package com.henrique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3UnitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3UnitTestApplication.class, args);
+	}
+
+}
---
+++ springboot3-unitTest/src/test/java/com/henrique/api/ApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.henrique.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.henrique.Springboot3UnitTestApplication;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void main() {
+    	Springboot3UnitTestApplication.main(new String[] {});
+    }
+
+}
